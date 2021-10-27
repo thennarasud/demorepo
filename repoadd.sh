24bc830428a6b03b6bb9332e9ac71b50a6ab44c1
EC2 Default User <ec2-user@ip-10-211-136-113.ap-southeast-2.compute.internal>
2021-10-27 08:30:43 +0000

    copyjob

---
+++ repoadd.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+ls
+git clone https://github.com/thennarasud/dot-net-core-sample-app.git
+ls
+git remote add origin https://github.com/thennarasud/demorepo.git
+git remote -v
+git init
+git add .
+git commit -m "copyjob"
+git branch -M main
